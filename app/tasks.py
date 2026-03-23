849b9a61774314f4e9532eb1006afe3e1be7a864
Moohaaamed <mohmaedaithmmadi@gmail.com>
2026-03-23 14:14:11 +0100

    Fix

--- app/tasks.py
+++ app/tasks.py
@@ -1,8 +1,11 @@
-tasks = ['task1','task2']
+tasks = ["task1", "task2"]
 
 def add_task(task):
-  tasks.append(task)
-  return task
+    tasks.append(task)
+    return task
 
 def get_tasks():
-  return tasks
+    return tasks
+
+def delete_task(task):
+    tasks.remove(task)
